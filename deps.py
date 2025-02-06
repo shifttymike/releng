203b5718f57ae24dd19add5f0e3a253e5e32affd
Michael Rodger <github@atinyhedgehog.za.net>
2025-02-06 09:17:43 +0200

    Update deps.py
    rip out s3

--- deps.py
+++ deps.py
@@ -246,15 +246,6 @@ def roll(bundle: Bundle,
         if e.code != 404:
             raise CommandError("network error") from e
 
-    s3_url = "s3://build.frida.re/deps/{version}/{filename}".format(version=version, filename=filename)
-
-    # We will most likely need to build, but let's check S3 to be certain.
-    r = subprocess.run(["aws", "s3", "ls", s3_url], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
-    if r.returncode == 0:
-        return
-    if r.returncode != 1:
-        raise CommandError(f"unable to access S3: {r.stdout.strip()}")
-
     artifact = build(bundle, build_machine, host_machine)
 
     if post is not None:
@@ -270,8 +261,6 @@ def roll(bundle: Bundle,
                            "--version=" + version,
                        ],
                        check=True)
-
-    subprocess.run(["aws", "s3", "cp", artifact, s3_url], check=True)
 
     # Use the shell for Windows compatibility, where npm generates a .bat script.
     subprocess.run("cfcli purge " + public_url, shell=True, check=True)
